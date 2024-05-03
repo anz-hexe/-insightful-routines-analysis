ad7b6a654d4f67bbeae5b42879acb745e2222c6b
AnzhelinaShevchuk <anzhelina.shevchuk@gmail.com>
2024-05-03 22:57:33 +0100

    feat: added config file with paths to the main files in the project

---
+++ insightful-routines-analysis/config.py
@@ -0,0 +1,7 @@
+PATH_TO_CSV = ""  # path to spreadsheet from Google Forms in format csv
+
+PROJECT_PATH = ""  # path to the project
+
+PHOTO_FOLDER_PATH = ""  # path to the folder wit face photo
+
+MODEL_PATH = ""  # path to the trained model

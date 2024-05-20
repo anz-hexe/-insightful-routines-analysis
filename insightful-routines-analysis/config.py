7caa14b3db79f7e534b56486eefbbbe585ab258b
AnzhelinaShevchuk <anzhelina.shevchuk@gmail.com>
2024-05-20 15:13:38 +0100

    fix: fixed folder name

--- insightful-routines-analysis/config.py
+++
@@ -1,7 +0,0 @@
-PATH_TO_CSV = ""  # path to spreadsheet from Google Forms in format csv
-
-PROJECT_PATH = ""  # path to the project
-
-PHOTO_FOLDER_PATH = ""  # path to the folder wit face photo
-
-MODEL_PATH = ""  # path to the trained model
